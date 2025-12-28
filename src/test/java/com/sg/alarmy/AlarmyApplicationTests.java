8ad7b90cf2724112eee5b40b1e8b63f1792177fb
snowtaro <ask0208ask@gmail.com>
2025-12-28 19:32:39 +0900

    initial commit

---
+++ src/test/java/com/sg/alarmy/AlarmyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sg.alarmy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlarmyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
